497ead20285374acea57c14b9f1754e2adafdbb2
ymor-calayag <jeffreyymor.calayag@gmail.com>
2026-01-05 15:05:18 +0800

    Add initial quiz app structure and question model

---
+++ main.py
@@ -0,0 +1,11 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_bank.append(Question(question["text"], question["answer"]))
+
+quiz_brain = QuizBrain(question_bank)
+print(quiz_brain.next_question())
