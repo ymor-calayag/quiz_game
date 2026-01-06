fcbb8035dc38d0bcbc0731467e4447669d383d2b
ymor-calayag <jeffreyymor.calayag@gmail.com>
2026-01-06 17:14:00 +0800

    Add new methods, finalize game functionality, added new question data.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from game_art import logo
+
+print(logo)
 
 question_bank = []
 
@@ -8,4 +11,6 @@ for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
 
 quiz_brain = QuizBrain(question_bank)
-print(quiz_brain.next_question())
+
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -2,8 +2,26 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
     
     def next_question(self):
-        user_answer = input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False)? ")
+        current_question = self.question_list[self.question_number]
+        user_answer = input(f"Q.{self.question_number + 1}: {current_question.text} (True/False)? ")
+        self.check_answer(user_answer, current_question) 
         self.question_number += 1
     
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+            print(f"You correctly answered {self.score} out of {len(self.question_list)} questions.")
+            return False
+        
+    def check_answer(self, user_answer, current_question):
+        if user_answer.lower() == current_question.answer.lower():
+            print("Your answer is correct")
+            self.score += 1
+        else:
+            print("Sorry, your answer is wrong")
+            print(f"The correct answer is {current_question.answer}")
+        print(f"Current score is: {self.score}\n")
